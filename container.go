990dd8b93774f984d58c9f3809c485af5d7a33c7
bitini111 <396039228@qq.com>
2023-04-12 14:19:33 +0800

    提交代码

---
+++ container.go
@@ -0,0 +1,68 @@
+package mvta
+
+import (
+	"fmt"
+	"github.com/bitini111/mvta/component"
+	"github.com/bitini111/mvta/config"
+	"github.com/bitini111/mvta/eventbus"
+	"github.com/bitini111/mvta/log"
+	"github.com/bitini111/mvta/task"
+	"runtime"
+
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type Container struct {
+	sig        chan os.Signal
+	components []component.Component
+}
+
+// NewContainer 创建一个容器
+func NewContainer() *Container {
+	return &Container{sig: make(chan os.Signal)}
+}
+
+// Add 添加组件
+func (c *Container) Add(components ...component.Component) {
+	c.components = append(c.components, components...)
+}
+
+// Serve 启动容器
+func (c *Container) Serve() {
+	log.Debug(fmt.Sprintf("Welcome to the due framework %s, Learn more at %s", Version, Website))
+
+	for _, comp := range c.components {
+		comp.Init()
+	}
+
+	for _, comp := range c.components {
+		comp.Start()
+	}
+
+	switch runtime.GOOS {
+	case `windows`:
+		signal.Notify(c.sig, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+	default:
+		signal.Notify(c.sig, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGABRT, syscall.SIGKILL, syscall.SIGTERM)
+	}
+
+	sig := <-c.sig
+
+	log.Warnf("process got signal %v, container will close", sig)
+
+	signal.Stop(c.sig)
+
+	for _, comp := range c.components {
+		comp.Destroy()
+	}
+
+	if err := eventbus.Close(); err != nil {
+		log.Errorf("eventbus close failed: %v", err)
+	}
+
+	task.Release()
+
+	config.Close()
+}
---
+++ version.go
@@ -0,0 +1,14 @@
+/**
+ * @Author: sheng
+ * @Email: 396039228@qq.com
+ * @Date: 2023/4/12 10:55 下午
+ * @Desc: TODO
+ */
+
+package mvta
+
+// Version 框架版本
+const Version = "v0.0.0.1"
+
+// Website 框架网址
+const Website = "https://github.com/bitini111/mvta"
